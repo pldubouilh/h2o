1808572bff8b31e8b9d5118a3b619ac5ab66a78e
Pierre Dubouilh <pldubouilh@gmail.com>
2019-05-12 21:19:31 +0200

    WIP: trace connections depending eBPF map
    after checking if a trace is enabled using the dtrace macro, it will also check if the connection ip is present on an eBPF map

--- include/h2o/probes.h
+++ include/h2o/probes.h
@@ -25,15 +25,109 @@
 /* This file must only be included from the source files of the h2o / libh2o, because H2O_USE_DTRACE is a symbol available only
  * during the build phase of h2o.  That's fine, because only h2o / libh2o should have the right to define probes belonging to the
  * h2o namespace.
- */
+*/
 #if H2O_USE_DTRACE
 
 #include "picotls.h"
 #include "h2o-probes.h"
 
-#define H2O_PROBE(label, ...)                                                                                                      \
+#ifdef __linux__
+#include <linux/bpf.h>
+#include <linux/unistd.h>
+
+struct keyType {
+    u_int8_t ipa[16];
+    u_int8_t ipb[16];
+    long porta;
+    long portb;
+};
+
+#define NR_CPU 4
+char name[] = "h2o_map";
+char path[] = "/sys/fs/bpf/h2o_map";
+static int map_fd;
+
+inline static void open_map() {
+    union bpf_attr attr;
+    int fd;
+
+    memset(&attr, 0, sizeof(attr));
+    attr.pathname = (__u64) (unsigned long)&path[0];
+
+    fd = syscall(__NR_bpf, BPF_OBJ_GET, &attr, sizeof(attr));
+    map_fd = fd; // __sync_lock_test_and_set
+}
+
+inline static int lookup_map(const void *key, const void *value) {
+    union bpf_attr attr;
+
+    memset(&attr, 0, sizeof(attr));
+    attr.map_fd = map_fd;
+    attr.key = (__u64) (unsigned long)key;
+    attr.value = (__u64) (unsigned long)value;
+
+    return syscall(__NR_bpf, BPF_MAP_LOOKUP_ELEM, &attr, sizeof(attr));
+}
+
+inline static void read_ip_port(struct sockaddr *sa, void *ip, long *port) {
+    if (sa->sa_family == AF_INET) {
+        struct sockaddr_in *sin = (void *)sa;
+        memcpy(ip, &sin->sin_addr, sizeof(sin->sin_addr));
+        // *port = sin->sin_port;
+        // printf("###### %d ", htons(sin->sin_port));
+     } else if (sa->sa_family == AF_INET6) {
+        struct sockaddr_in6 *sin = (void *)sa;
+        memcpy(ip, &sin->sin6_addr, sizeof(sin->sin6_addr));
+        // *port = sin->sin6_port;
+        // printf("###### %d\r\n", htons(sin->sin6_port));
+    }
+}
+
+inline static int check_map(h2o_socket_t *sock)
+{
+    // check if map opened
+    if (map_fd <= 0) {
+        open_map();
+        if (map_fd <= 0) return 1; // map can't be opened, fallback accepting probe
+    }
+
+    struct sockaddr_storage loc;
+    struct sockaddr_storage rem;
+    struct keyType key;
+    __u64 vals[NR_CPU];
+    memset(&vals, 0, sizeof(vals));
+    memset(&key, 0, sizeof(key));
+
+    // get sock/peer ip/ports
+    h2o_socket_getsockname(sock, (void *)&loc);
+    h2o_socket_getpeername(sock, (void *)&rem);
+
+    // read ip/ports, put parsed val into key structure
+    read_ip_port((void *)&loc, &key.ipa, &key.porta);
+    read_ip_port((void *)&rem, &key.ipb, &key.portb);
+
+    // lookup map for our key
+    lookup_map(&key, &vals);
+
+    // printf("~~~~~~~~~~~~~~~~~~~~~~~~~~ {");
+    // for(int i=0; i < NR_CPU; i++) printf("%lld,", vals[i]);
+    // printf("} \r\n");
+
+    // return 1 if value present in map
+    for(int i=0; i < NR_CPU; i++) if (vals[i] > 0) return 1;
+
+    // ... or 0 otherwise
+    return -1;
+}
+#else
+inline static int check_map(h2o_socket_t *sock) {
+    return 1;
+}
+#endif
+
+#define H2O_PROBE(label, sock, ...)                                                                                                \
     do {                                                                                                                           \
-        if (PTLS_UNLIKELY(H2O_H2O_##label##_ENABLED())) {                                                                          \
+        if (PTLS_UNLIKELY(H2O_H2O_##label##_ENABLED()) && check_map(sock) != -1) {                                                 \
             H2O_H2O_##label(__VA_ARGS__);                                                                                          \
         }                                                                                                                          \
     } while (0)
@@ -42,12 +136,10 @@
         size_t _l = (l);                                                                                                           \
         ptls_hexdump(alloca(_l * 2 + 1), (s), _l);                                                                                 \
     })
-
 #else
 
 #define H2O_PROBE(label, ...)
 #define H2O_PROBE_HEXDUMP(s, l)
 
 #endif
-
 #endif
